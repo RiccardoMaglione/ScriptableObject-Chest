d1bc8e9b4c5bf37a4834017b96a55484b57ed42b
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-12-22 21:54:41 +0100

    Add info panel and reload scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public GameObject PanelChest;
+    public GameObject PanelInfo;
+
+    public void ChestToInfo()
+    {
+        PanelChest.SetActive(false);
+        PanelChest.SetActive(true);
+    }
+
+    public void InfoToChest()
+    {
+        PanelChest.SetActive(true);
+        PanelChest.SetActive(false);
+    }
+
+    public void ReloadScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
